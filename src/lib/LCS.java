656f9155789bf353859d5c0b5a7844cf19417528
Dago Palmera <dagopalmera1@gmail.com>
2025-03-30 12:25:44 -0500

    Se creo la clase LCS, esta clase contiene el algoritmo de Longest common Subsequence que se usa para analizar las cadenas de ADN

---
+++ src/lib/LCS.java
@@ -0,0 +1,49 @@
+package lib;
+
+public class LCS {
+    
+    // Método para encontrar la subsecuencia común más larga (LCS)
+    public static String findLCS(String X, String Y) {
+
+        int m = X.length();
+        int n = Y.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Llenando la tabla de programación dinámica
+        for (int i = 1; i <= m; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // Reconstrucción de la LCS desde la tabla dp
+        StringBuilder lcs = new StringBuilder();
+        int i = m, j = n;
+
+        while (i > 0 && j > 0) {
+
+            if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+
+                lcs.append(X.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+
+                i--;
+            } else {
+                
+                j--;
+            }
+        }
+
+        return lcs.reverse().toString();
+    }
+}
